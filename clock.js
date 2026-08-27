0d97408d3b22482aa46599da53f957ebfa00f151
ParbinShrees <parbinshreesh64487@gmail.com>
2026-08-27 23:22:01 +0545

    added clock.js

--- clock.js
+++ clock.js
@@ -3,36 +3,13 @@ const clock = document.getElementById("time");
 const updateClock = () => {
     const date = new Date();
 
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
 
     clock.textContent = `${hours}:${minutes}:${seconds}`;
 };
 
-updateClock();          // Show time immediately
+updateClock();
+
 setInterval(updateClock, 1000);
-
-// const d = new Date("October 13, 2014 11:13:00");
-
-// document.getElementById("demo").innerHTML = d;
-
-
-
-// function updateClock() {
-//     const now = new Date();
-
-//     const hours = String(now.getHours()).padStart(2, "0");
-//     const minutes = String(now.getMinutes()).padStart(2, "0");
-//     const seconds = String(now.getSeconds()).padStart(2, "0");
-
-//     document.getElementById("time").textContent =
-//         `${hours}:${minutes}:${seconds}`;
-// }
-
-// updateClock();          // Run immediately
-// setInterval(updateClock, 1000); // Update every second
-
-// const d = new Date();
-
-// console.log(d);
