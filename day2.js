e73cf2bf0c35407e6e75e0554e4012b6a9f12e96
ParbinShrees <parbinshreesh64487@gmail.com>
2026-08-27 23:20:33 +0545

    added index.html for clock.js

--- day2.js
+++ day2.js
@@ -1,54 +1,54 @@
 const fruits = ["apple", "banana", "cherry", "date", "Mango"]
-// console.log(fruits[0])
-// console.log(fruits[2])
-// console.log(fruits.length) //Array length
-// console.log(fruits.toString()) //Array toString()
-// console.log(fruits.at(2)) //JavaScript Array at()
-// console.log(fruits[2]) //JavaScript Array at()
+console.log(fruits[0])
+console.log(fruits[2])
+console.log(fruits.length) //Array length
+console.log(fruits.toString()) //Array toString()
+console.log(fruits.at(2)) //JavaScript Array at()
+console.log(fruits[2]) //JavaScript Array at()
 
-// //JavaScript Array join()
-// console.log(fruits.join("-"))
+//JavaScript Array join()
+console.log(fruits.join("-"))
 
-// //pop()
-// fruits.pop()
-// console.log(fruits);
+//pop()
+fruits.pop()
+console.log(fruits);
 
-// //push()
-// fruits.push("Cherry")
-// console.log(fruits);
+//push()
+fruits.push("Cherry")
+console.log(fruits);
 
-// //shift()
-// fruits.shift()
-// console.log(fruits)
+//shift()
+fruits.shift()
+console.log(fruits)
 
-// //unshift()
-// fruits.unshift("Apple")
-// console.log(fruits)
+//unshift()
+fruits.unshift("Apple")
+console.log(fruits)
 
-// //Array.isArray()
-// console.log(Array.isArray(fruits))
+Array.isArray()
+console.log(Array.isArray(fruits))
 
-// //delete
-// delete fruits[1];
+//delete
+delete fruits[1];
 
-// console.log(fruits);
-// console.log(fruits.length);
+console.log(fruits);
+console.log(fruits.length);
 
-// //concat()
+//concat()
 
-// const fruits1 = ["apple", "banana", "cherry"]
-// const food = fruits.concat(fruits1)
-// console.log(food)
+const fruits1 = ["apple", "banana", "cherry"]
+const food = fruits.concat(fruits1)
+console.log(food)
 
-//Array copywithin()
-//fruits.copyWithin(2, 0)
-//console.log(fruits)
+Array copywithin()
+fruits.copyWithin(2, 0)
+console.log(fruits)
 
-//Array flat()
-//const arr = [1,2, [4,5,6], [7,8,9]];
-//console.log (arr.flat());
+Array flat()
+const arr = [1,2, [4,5,6], [7,8,9]];
+console.log (arr.flat());
 
-//Array flatMap()
+Array flatMap()
 const myArr = [1, 3, 4, 5, 6,7,8,9,];
 
 const newArr = myArr.flatMap((num) => [num * 2]);
